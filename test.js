1e7cc1f68ff09536fc3e253cfc40c9d3211c3ed6
Jinbo Huang <hnsxxscyx@gmail.com>
2021-03-21 19:24:11 +0800

    feat: remove useless code

--- test.js
+++
@@ -1,9 +0,0 @@
-import { TestDriver } from './testCase/test/index.js';
-
-export const testFunc = async (driver, baseUrl) => {
-	driver.get(baseUrl);
-	let testInstance;
-	// test
-	testInstance = new TestDriver({ driver, baseUrl });
-	await testInstance.test();
-};
